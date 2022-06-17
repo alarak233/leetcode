8a0bfa684a9c9f13387979617e16138e91db08d1
alarak233 <727072895@qq.com>
2022-06-17 08:21:46 +0800

    1089

---
+++ 1089.复写零.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=1089 lang=cpp
+ *
+ * [1089] 复写零
+ */
+
+// @lc code=start
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int zeroNum =0;
+        for(auto i:arr){
+            if(i==0)zeroNum++;
+        }
+
+        int len=arr.size();
+
+        arr.resize(arr.size()+zeroNum,0);
+
+        for(int i=arr.size()-1-zeroNum;i>=0;i--){
+            if(arr[i]!=0){
+                arr[i+zeroNum]=arr[i];
+            }
+            else{
+                arr[i+zeroNum]=0;
+                arr[i+zeroNum-1]=0;
+                zeroNum--;
+            }
+        }
+
+        arr.resize(len);
+    }
+};
+// @lc code=end
+
