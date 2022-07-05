f75e2f4e091f96df508d3d735a18a6172f3192a4
alarak233 <727072895@qq.com>
2022-07-05 13:05:49 +0800

    729

---
+++ 729.我的日程安排表-i.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=729 lang=cpp
+ *
+ * [729] 我的日程安排表 I
+ */
+
+// @lc code=start
+#include<set>
+using namespace std;
+
+class MyCalendar {
+    set<pair<int, int>> booked;
+
+public:
+    bool book(int start, int end) {
+        auto it = booked.lower_bound({end, 0});
+        if (it == booked.begin() || (--it)->second <= start) {
+            booked.emplace(start, end);
+            return true;
+        }
+        return false;
+    }
+};
+
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
+// @lc code=end
+
