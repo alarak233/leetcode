b7153067632d6f3fe5c974d7bed3b33120ec11d7
alarak233 <727072895@qq.com>
2022-07-22 08:22:25 +0800

    814

---
+++ 814.二叉树剪枝.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=814 lang=cpp
+ *
+ * [814] 二叉树剪枝
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root->left!=nullptr) root->left=pruneTree(root->left);
+        if(root->right!=nullptr) root->right=pruneTree(root->right);
+        if(root->left==nullptr&&root->right==nullptr)return (root->val==1)?root:nullptr;
+        return root;
+    }
+};
+// @lc code=end
+
