3848954e8f408e0d80cfda3460fc018d4fc2133c
alarak233 <727072895@qq.com>
2023-01-06 08:28:38 +0800

    1802

---
+++ src/1802.有界数组中指定下标处的最大值.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=1802 lang=cpp
+ *
+ * [1802] 有界数组中指定下标处的最大值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maxValue(int n, int index, int maxSum) {
+        int left=0;
+        int right=maxSum;
+        int ans=(left+right)/2;
+        while(left<right){
+            int sum=0;
+            
+        }
+    }
+};
+// @lc code=end
+
