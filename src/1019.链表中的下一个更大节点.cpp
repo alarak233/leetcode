f092dada4c83dee3806c6f870c77ff33201487c6
alarak233 <727072895@qq.com>
2023-04-10 08:36:48 +0800

    1019 单调栈

---
+++ src/1019.链表中的下一个更大节点.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=1019 lang=cpp
+ *
+ * [1019] 链表中的下一个更大节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include<vector>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> ans;
+        stack<pair<int, int>> s;
+
+        ListNode* cur = head;
+        int idx = -1;
+        while (cur) {
+            ++idx;
+            ans.push_back(0);
+            while (!s.empty() && s.top().first < cur->val) {
+                ans[s.top().second] = cur->val;
+                s.pop();
+            }
+            s.emplace(cur->val, idx);
+            cur = cur->next;
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
